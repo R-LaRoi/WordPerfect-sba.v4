47b9fffe48edbe0ab8f9a56c4b6c93da7ad11dc1
R. LaRoi <are@raewhoo.com>
2024-06-24 23:13:47 -0400

    ReadMe

---
+++ ReadMe.md
@@ -0,0 +1,29 @@
+# WORDPERFECT - sba.v4
+
+A Javascript application that allows users to generate a word of the day, a word cloud and search for the definintion of a word.
+
+## Objectives
+
+- Use asynchronous JavaScript tools to build a responsive web application.
+- Demonstrate understanding of the JavaScript event loop.
+- Generate asynchronous code using promises and async/await syntax.
+- Use fetch and/or Axios to interact with an external web API.
+- Organize files using modules and imports.
+
+## Features
+
+- Word of The Day API
+- Dictionary API
+- Word Cloud API
+
+## Tech Stack
+
+**Client:** Javascript, HTML, CSS
+
+## Screenshots
+
+![App Screenshot](https://github.com/R-LaRoi/WordPerfect-sba.v4/assets/114012059/ef141bb6-212d-4f18-ad51-bb0a30f6af02)
+
+![App Screenshot](https://github.com/R-LaRoi/WordPerfect-sba.v4/assets/114012059/860b7dfc-5be3-462d-8aa0-fcb9b057e0d1)
+
+![App Screenshot](https://github.com/R-LaRoi/WordPerfect-sba.v4/assets/114012059/934b5ec7-4fc5-4972-9bf2-1fb7cc239890)
--- src/script.js
+++ src/script.js
@@ -1,38 +1,46 @@
 let wcUserText = document.getElementById("cloud-text");
 let userWord = document.getElementById("user-word");
 let newWord = document.getElementById("new-word-btn");
-let wordList = document.getElementById("saved-words");
+let savedWords = document.getElementById("saved-words");
+let showWords = document.getElementById("show-words");
 let API_KEY = "d005d2a608msha616720d96273b5p17058fjsn3a4d7e3b9415";
+let listing = document.getElementById("show-list");
 
+// c9aMxyHrR0kU5Czfop6UOA == RWb8VOQ7cm0kWmwf;
 // Word of the day API ---------
 // Defines word of the day using Dictionary API -------
 
-// function wordOfTheDay() {
-//   let wordOfDay = document.getElementById("word-of-day");
-//   fetch("https://random-word-api.herokuapp.com/word?number=1")
-//     .then((response) => response.json())
-//     .then((data) => {
-//       console.log(data);
-//       wordOfDay.innerText = data;
-//       fetch(
-//         `https://www.dictionaryapi.com/api/v3/references/collegiate/json/${data}?key=44793c76-dae0-4101-a109-1642ae60ecea`
-//       )
-//         .then((response) => response.json())
-//         .then((define) => {
-//           console.log(define[0]);
-//           if (!define[0]) {
-//             document.getElementById(
-//               "wod-define"
-//             ).innerText = `No definition provided for ${data}.`;
-//           } else {
-//             document.getElementById("wod-define").innerText =
-//               define[0].shortdef[0];
-//           }
-//         });
-//     });
-// }
+function wordOfTheDay() {
+  let wordOfDay = document.getElementById("word-of-day");
+  // fetch("https://random-word-api.herokuapp.com/word?number=1")
+  fetch("https://api.api-ninjas.com/v1/randomword", {
+    headers: { "X-Api-Key": "4oizHQmbKbbQVNBZoHENP5tGR5mgpaIAucOaRp2T" },
+    contentType: "application/json",
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data.word);
+      let dayWord = data.word;
+      wordOfDay.innerText = dayWord;
+      fetch(
+        `https://www.dictionaryapi.com/api/v3/references/collegiate/json/${dayWord}?key=44793c76-dae0-4101-a109-1642ae60ecea`
+      )
+        .then((response) => response.json())
+        .then((define) => {
+          console.log(define[0]);
+          if (define[0].shortdef[0] === "") {
+            document.getElementById(
+              "wod-define"
+            ).innerText = `No definition provided for ${data}.`;
+          } else {
+            document.getElementById("wod-define").innerText =
+              define[0].shortdef[0];
+          }
+        });
+    });
+}
 
-// wordOfTheDay();
+wordOfTheDay();
 
 // generates new word of the day
 newWord.addEventListener("click", (e) => {
@@ -55,8 +63,6 @@ function searchWord() {
     let showDefinition = document.getElementById("show-def");
     let showExample = document.getElementById("show-ex");
     let addWordBtn = document.getElementById("add-word-btn");
-    addWordBtn.style.display = "hidden";
-    showExample.style.fontSize = "1rem";
 
     // async await function ----
     async function defineWord() {
@@ -67,24 +73,26 @@ function searchWord() {
           if (!data[0].meanings[0].definitions[0].definition) {
             showDefinition.innerHTML = `No example provided for ${keyword}.`;
           } else {
-            showDefinition.innerHTML =
-              data[0].meanings[0].definitions[0].definition;
+            showDefinition.innerHTML = `Definition: <i>${data[0].meanings[0].definitions[0].definition}</i> <br>`;
+
+            addWordBtn.addEventListener("click", (e) => {
+              e.preventDefault();
+              addToList();
+            });
           }
 
           if (!data[0].meanings[0].definitions[0].example) {
             showExample.innerText = `No example provided for ${keyword}.`;
           } else {
-            showExample.innerText = data[0].meanings[0].definitions[0].example;
+            showExample.innerText = `Example: ${data[0].meanings[0].definitions[0].example}`;
           }
 
-          addWordBtn.addEventListener("click", (e) => {
-            e.preventDefault();
-            console.log();
-            let saveWord = document.createElement("div");
-            saveWord.innerHTML = `You liked ${keyword}. Definition: ${data[0].meanings[0].definitions[0].definition}`;
-
-            wordList.append(saveWord);
-          });
+          function addToList() {
+            let thisItem = document.createElement("li");
+            thisItem.classList.add("list-item");
+            thisItem.textContent = `${keyword}`;
+            listing.append(thisItem);
+          }
         })
         .catch((err) => {
           console.log(err);
@@ -117,8 +125,8 @@ async function makeWordCloud() {
         body: JSON.stringify({
           text: userCloud,
           format: "svg",
-          width: 700,
-          height: 800,
+          width: 500,
+          height: 500,
           fontFamily: "sans-serif",
           fontScale: 75,
           scale: "linear",
